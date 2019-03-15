c61a61f58951261195d9084eb7910a27e44f9df0
dqu <dqu@biba.uni-bremen.de>
2019-03-15 16:30:40 +0100

    - updated ssl enabled mongodb connection
    - support ssl eneable mongodb

--- epcis_nimble_server/src/main/java/eu/nimble/service/epcis/db/MongoConfig.java
+++ epcis_nimble_server/src/main/java/eu/nimble/service/epcis/db/MongoConfig.java
@@ -1,8 +1,20 @@
 package eu.nimble.service.epcis.db;
 
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+//import javax.security.cert.X509Certificate;
 
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.ssl.TrustStrategy;
@@ -19,6 +31,8 @@ import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoDatabase;
 
 /**
@@ -29,78 +43,101 @@ import com.mongodb.client.MongoDatabase;
 public class MongoConfig {
     private static Logger log = LoggerFactory.getLogger(MongoConfig.class);
 
-//    @Value("${spring.data.mongodb.host}")
-//    public String mongoHost;
-//
-//    @Value("${spring.data.mongodb.port}")
-//    public String mongoPort;
-
     @Value("${spring.data.mongodb.database}")
     public String mongoDB;
     
     @Value("${spring.data.mongodb.uri}")
     public String mongoURI;
     
-
+    @Value("${spring.data.mongodb.ssl}")
+    public boolean sslEnabled;
+    
     /**
      * MongoTemplate Bean
      * @param mongoDbFactory
      * @return
+     * @throws NoSuchAlgorithmException 
+     * @throws UnknownHostException 
+     * @throws KeyManagementException 
      */
     @Bean
-    public MongoOperations mongoTemplate(){
+    public MongoOperations mongoTemplate() throws KeyManagementException, UnknownHostException, NoSuchAlgorithmException{
         return new MongoTemplate(mongoDbFactory());
     }
     
     /**
      * MongoDbFactory bean
      * @return
+     * @throws NoSuchAlgorithmException 
+     * @throws UnknownHostException 
+     * @throws KeyManagementException 
      */
     @Bean
-    public MongoDbFactory mongoDbFactory(){
+    public MongoDbFactory mongoDbFactory() throws KeyManagementException, UnknownHostException, NoSuchAlgorithmException{
         return new SimpleMongoDbFactory(mongoClient(), mongoDB);
     }
     
-    /**
-     * MongoClient bean
-     * @return
-     */
-    @Bean
-    public MongoClient mongoClient(){
-    	
-//    	if(mongoURI == null || mongoURI.isEmpty())
-//    	{
-//    		return  new MongoClient(mongoHost + ":" + mongoPort);
-//    	}
-    	
-        try {
-            SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
-                @Override
-                public boolean isTrusted(java.security.cert.X509Certificate[] x509Certificates, String s) throws CertificateException {
-                    return true;
-                }
-            }).build();
-            
-            MongoClientOptions.Builder builder = new MongoClientOptions.Builder().sslEnabled(true).sslInvalidHostNameAllowed(true).socketFactory(sslContext.getSocketFactory());
-            MongoClientURI connectionString = new MongoClientURI(mongoURI, builder);
-            
-            log.info("Connection String: " +  connectionString + ", database name: " + mongoDB);
-            return new MongoClient(connectionString);
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
+    @Bean
+    public MongoClient mongoClient() throws UnknownHostException, NoSuchAlgorithmException, KeyManagementException{
+        
+        MongoClientURI mongoClientURI = new MongoClientURI(mongoURI);
+        
+
+        MongoClientOptions options = MongoClientOptions.builder().build();
+        if(sslEnabled)
+        {
+        	options = getSSLEnabledMongoClientOptions();
         }
         
-        return null;
+        
+        List<ServerAddress> serverAddressList = mongoClientURI.getHosts().stream().map(host -> new ServerAddress(host)).collect(Collectors.toList());
+        
+        List<MongoCredential> credentialList = new ArrayList<MongoCredential>();
+        MongoCredential credential = mongoClientURI.getCredentials();
+        if(null != credential)
+        {
+        	credentialList.add(credential);
+        }
+        
+
+        MongoClient mongoClient = new MongoClient(serverAddressList, credentialList, options); 		
+                
+        return mongoClient;
+    }
+
+    private MongoClientOptions getSSLEnabledMongoClientOptions()throws UnknownHostException, NoSuchAlgorithmException, KeyManagementException{
+    	
+        TrustManager[] trustManagers = new TrustManager[] {
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() { return null; }
+                    public void checkClientTrusted(X509Certificate[] certs, String t) { }
+                    public void checkServerTrusted(X509Certificate[] certs, String t) { }
+                }
+            };
+
+            SSLContext sslContext=SSLContext.getInstance("TLS");
+            sslContext.init(null,trustManagers,new SecureRandom());
+            
+            MongoClientOptions options = MongoClientOptions.builder().
+                            sslEnabled(true).
+                            sslInvalidHostNameAllowed(true).
+                            socketFactory(sslContext.getSocketFactory()).
+                            build();
+            
+            return options;
     }
     
     
     /**
      * MongoDatabase bean
      * @return
+     * @throws NoSuchAlgorithmException 
+     * @throws UnknownHostException 
+     * @throws KeyManagementException 
      */
     @Bean
-    public MongoDatabase mongoDatabase()
+    public MongoDatabase mongoDatabase() throws KeyManagementException, UnknownHostException, NoSuchAlgorithmException
     {
     	return mongoClient().getDatabase(mongoDB);
     }
