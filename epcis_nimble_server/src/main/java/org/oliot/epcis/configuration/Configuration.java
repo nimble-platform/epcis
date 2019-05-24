a51f5f28acc86a2f56feaaa64e555a1140856f2c
nazmulbiba <hos.biba.1994@gmail.com>
2019-05-24 19:11:44 +0200

    merge

--- epcis_nimble_server/src/main/java/org/oliot/epcis/configuration/Configuration.java
+++ epcis_nimble_server/src/main/java/org/oliot/epcis/configuration/Configuration.java
@@ -43,5 +43,5 @@ public class Configuration {
     public void setDatabase(boolean isCaptureVerfificationOn) {
     	Configuration.isCaptureVerfificationOn = isCaptureVerfificationOn;
     }
-    
+
 }
