c8bc1918c2519dbc5c5fbc9e4546601ac86995c3
nazmulbiba <hos.biba.1994@gmail.com>
2019-06-27 18:09:08 +0200

    add mock data for testing into json production proc template

--- epcis_nimble_server/src/test/java/org/oliot/epcis/service/JsonProductionProcTemplateTest.java
+++ epcis_nimble_server/src/test/java/org/oliot/epcis/service/JsonProductionProcTemplateTest.java
@@ -8,8 +8,6 @@ import org.oliot.epcis.service.capture.mongodb.MongoCaptureUtil;
 import org.oliot.epcis.service.query.mongodb.MongoQueryService;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
@@ -19,9 +17,8 @@ public class JsonProductionProcTemplateTest {
     MongoCaptureUtil mongoCaptureUtil = new MongoCaptureUtil();
     MongoQueryService mongoQueryService = new MongoQueryService();
 
-    @Test
-    public void captureJSONProductionProcTemplate() {
-        String productProcessTemplateJson = "{\n" +
+        String token = "testUser";
+        String testJson = "{\n" +
                 "  \"productClass\": \"testProductionClass\",\n" +
                 "  \"productionProcessTemplate\": [\n" +
                 "    {\n" +
@@ -33,39 +30,71 @@ public class JsonProductionProcTemplateTest {
                 "      \"hasNext\": \"2\"\n" +
                 "    },\n" +
                 "    {\n" +
-                "      \"id\": \"4\",\n" +
-                "      \"hasPrev\": \"3\",\n" +
+                "      \"id\": \"2\",\n" +
+                "      \"hasPrev\": \"1\",\n" +
                 "      \"readPoint\": \"urn:epc:id:sgln:readPoint.lindbacks.4\",\n" +
                 "      \"bizLocation\": \"urn:epc:id:sgln:bizLocation.lindbacks.5\",\n" +
                 "      \"bizStep\": \"urn:epcglobal:cbv:bizstep:shipping\",\n" +
-                "      \"hasNext\": \"\"\n" +
+                "      \"hasNext\": \"3\"\n" +
                 "    }\n" +
                 "  ]\n" +
                 "}";
-        JSONObject jsonProductionProc = new JSONObject(productProcessTemplateJson);
-        mongoCaptureUtil.captureJSONProductionProcTemplate(jsonProductionProc, "token1");
+        String resultTestJson = "[\n" +
+                " {\n" +
+                "  \"bizStep\": \"urn:epcglobal:cbv:bizstep:other\",\n" +
+                "  \"hasPrev\": \"\",\n" +
+                "  \"readPoint\": \"urn:epc:id:sgln:readPoint.lindbacks.1\",\n" +
+                "  \"bizLocation\": \"urn:epc:id:sgln:bizLocation.lindbacks.2\",\n" +
+                "  \"hasNext\": \"2\",\n" +
+                "  \"id\": \"1\"\n" +
+                " },\n" +
+                " {\n" +
+                "  \"bizStep\": \"urn:epcglobal:cbv:bizstep:shipping\",\n" +
+                "  \"hasPrev\": \"1\",\n" +
+                "  \"readPoint\": \"urn:epc:id:sgln:readPoint.lindbacks.4\",\n" +
+                "  \"bizLocation\": \"urn:epc:id:sgln:bizLocation.lindbacks.5\",\n" +
+                "  \"hasNext\": \"3\",\n" +
+                "  \"id\": \"2\"\n" +
+                " }\n" +
+                "]";
+
+    /**
+     * @Test
+     * Capture Json Production Process Template and
+     * compare with resultTestJson
+     */
+    @Test
+    public void captureJSONProductionProcTemplate() {
+        JSONObject jsonProductionProc = new JSONObject(testJson);
+        mongoCaptureUtil.captureJSONProductionProcTemplate(jsonProductionProc, token);
         try {
-            assertFalse(mongoQueryService.pollProductionProcTemplateQuery("testProductionClass").contains("[]"));
+            assertTrue(mongoQueryService.pollProductionProcTemplateQuery("testProductionClass").equals(resultTestJson));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * @Test
+     * This class "test" does not have entry on database
+     */
     @Test
-    // this class "test" does not have entry on database
     public void getProductionProcessTemplateWithoutClass() {
         try {
-            assertTrue(mongoQueryService.pollProductionProcTemplateQuery("test").contains("[]"));
+            assertTrue(mongoQueryService.pollProductionProcTemplateQuery("test").equals("[]"));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * @Test
+     * This class "testProductionClass" have entry of database
+     */
     @Test
-    // this class "testProductionClass" have entry of database
     public void getProductionProcessTemplateWithClass() {
         try {
-            assertFalse(mongoQueryService.pollProductionProcTemplateQuery("testProductionClass").contains("[]"));
+            assertTrue(mongoQueryService.pollProductionProcTemplateQuery("testProductionClass").equals(resultTestJson));
         } catch (Exception e) {
             e.printStackTrace();
         }
