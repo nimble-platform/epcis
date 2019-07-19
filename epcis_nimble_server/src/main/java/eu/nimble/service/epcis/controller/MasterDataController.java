69f6f16373afc287e7a92696b05896daf32230d9
nazmulbiba <hos.biba.1994@gmail.com>
2019-07-19 14:42:57 +0200

    add api for delete master data and doc

--- epcis_nimble_server/src/main/java/eu/nimble/service/epcis/controller/MasterDataController.java
+++ epcis_nimble_server/src/main/java/eu/nimble/service/epcis/controller/MasterDataController.java
@@ -1,14 +1,19 @@
 package eu.nimble.service.epcis.controller;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
 import eu.nimble.service.epcis.services.AuthorizationSrv;
 import io.swagger.annotations.*;
 import org.bson.BsonDocument;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 import org.oliot.epcis.configuration.Configuration;
 import org.oliot.epcis.converter.mongodb.model.MasterData;
+import org.oliot.epcis.service.query.RESTLikeQueryService;
+import org.oliot.epcis.service.query.mongodb.MongoQueryService;
+import org.oliot.model.epcis.PollParameters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,13 +31,9 @@ public class MasterDataController extends BaseRestController{
 
     private static Logger log = LoggerFactory.getLogger(MasterDataController.class);
 
-    @ApiOperation(value = "Get MasterData item for the given ObjectId.", notes = "Return one MasterData Item based on ObjectId, which is the unique id of MasterData Table",
-            response = org.oliot.epcis.converter.mongodb.model.MasterData.class, responseContainer="List")
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "success"),
-            @ApiResponse(code = 401, message = "Unauthorized. Are the headers correct?"), })
+    @ApiOperation( value = "", hidden = true)
     @GetMapping("/GetMasterDataItem")
-    public ResponseEntity<?> getMasterDataItem(@ApiParam(value = "MasterData ObjectId in NIMBLE Platform", required = true) @RequestParam("id") String id,
-           @ApiParam(value = "The Bearer token provided by the identity service", required = true)
+    public ResponseEntity<?> getMasterDataItem(@RequestParam("id") String id,
            @RequestHeader(value="Authorization", required=true) String bearerToken) {
 
         // Check NIMBLE authorization
@@ -51,15 +52,15 @@ public class MasterDataController extends BaseRestController{
         return new ResponseEntity<>(masterDataItem.toJson(), responseHeaders, HttpStatus.OK);
     }
 
-    @ApiOperation(value = "Delete MasterData item for the given ObjectId.",
-            notes = "Delete one MasterData Item based on ObjectId, which is the unique id of MasterData Table", response = String.class)
+    @ApiOperation(value = "Delete MasterData items for the given Id.",
+            notes = "Delete all MasterData Items match on given Id", response = String.class)
     @ApiResponses(value = { @ApiResponse(code = 200, message = "success"),
             @ApiResponse(code = 400, message = "ObjectId is not valid?"),
             @ApiResponse(code = 401, message = "Unauthorized. Are the headers correct?"), })
-    @PostMapping("/deleteMasterDataItem")
-    public ResponseEntity<?> deleteMasterDataItem(@ApiParam(value = "MasterData ObjectId", required = true) @RequestParam("id") String id,
-              @ApiParam(value = "The Bearer token provided by the identity service", required = true)
-              @RequestHeader(value="Authorization", required=true) String bearerToken) {
+    @PostMapping("/deleteMasterDataItemById")
+    public ResponseEntity<?> deleteMasterDataItemById(@ApiParam(value = "MasterData Item Id", required = true) @RequestParam("id") String id,
+                                                  @ApiParam(value = "The Bearer token provided by the identity service", required = true)
+                                                  @RequestHeader(value="Authorization", required=true) String bearerToken) {
 
         // Check NIMBLE authorization
         String userPartyID = authorizationSrv.checkToken(bearerToken);
@@ -69,8 +70,43 @@ public class MasterDataController extends BaseRestController{
 
         MongoCollection<BsonDocument> collection = Configuration.mongoDatabase.getCollection("MasterData",
                 BsonDocument.class);
-        collection.deleteOne(new Document("_id", new ObjectId(id)));
-        log.info("Delete Master Data Item Id: " + id);
-        return new ResponseEntity<>("Delete Master Data Item Id: " + id, HttpStatus.OK);
+        BasicDBObject document = new BasicDBObject();
+        document.append("id", id);
+        collection.deleteMany(document);
+
+        log.info("Delete All Master Data Items for Id: " + id);
+        return new ResponseEntity<>("Delete All Master Data Items for Id: " + id, HttpStatus.OK);
+    }
+
+
+    @ApiOperation(value = "Get MasterData item for the given id.", notes = "Return one MasterData Item based on id",
+            response = org.oliot.epcis.converter.mongodb.model.MasterData.class, responseContainer="List")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "success"),
+            @ApiResponse(code = 401, message = "Unauthorized. Are the headers correct?"), })
+    @GetMapping("/GetLatestMasterDataById")
+    public ResponseEntity<?> getLatestMasterDataItemById(@ApiParam(value = "MasterData id in NIMBLE Platform", required = true) @RequestParam("id") String id,
+                                               @ApiParam(value = "The Bearer token provided by the identity service", required = true)
+                                               @RequestHeader(value="Authorization", required=true) String bearerToken) {
+
+        // Check NIMBLE authorization
+        String userPartyID = authorizationSrv.checkToken(bearerToken);
+        if (userPartyID == null) {
+            return new ResponseEntity<>(new String("Invalid AccessToken"), HttpStatus.UNAUTHORIZED);
+        }
+
+        PollParameters p = new PollParameters();
+        p.setMaxElementCount(1);
+        p.setEQ_name(id);
+        p.setMasterDataFormat("JSON");
+        p.setQueryName("SimpleMasterDataQuery");
+        String masterDataItem = null;
+        MongoQueryService mongoQueryService = new MongoQueryService();
+        try {
+            masterDataItem = mongoQueryService.poll(p, userPartyID);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>(masterDataItem, HttpStatus.OK);
     }
 }
